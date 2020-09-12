47f9fbb2642588c7c020fb66b81cfeb8389ecf5e
ipixel <ipixel@kangxin.com>
2020-09-12 20:41:15 +0800

    SpringBoot Shiro Oauth2 Client Demo

---
+++ src/main/java/com/solvay/shiro/SpringbootShiroClientApplication.java
@@ -0,0 +1,13 @@
+package com.solvay.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShiroClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootShiroClientApplication.class, args);
+    }
+
+}
